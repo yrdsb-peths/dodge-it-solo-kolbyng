e3f8b109d0081b3e728bcf2404728cfcc5685d1d
kolbyng <440021056@gapps.yrdsb.ca>
2025-11-06 14:27:47 -0500

    Added full game

---
+++ Banana.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Banana extends Actor
+{
+    public void act(){
+        move(-3);
+        
+        if (getX() <= 0){
+            resetBanana();
+        }
+        
+        if (isTouching(Hero.class)){
+            World world = getWorld();
+            world.removeObject(this);
+            
+            SadFace sadFace = new SadFace();
+            world.addObject(sadFace, world.getWidth() / 2, world.getHeight() / 2);
+            Greenfoot.stop();
+        }
+    }
+    
+    public void resetBanana(){
+        int num = Greenfoot.getRandomNumber(2);
+        int x = 600;
+        int y;
+        
+        if (num == 0){
+            y = 100;
+        } else{
+            y = 300;
+        }
+        
+        setLocation(x, y);
+    }
+}
